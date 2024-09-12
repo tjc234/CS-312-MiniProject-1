7a15224184f03f835e10ba21dad1d49cca1c26cc
tjc234 <tjc234@nau.edu>
2024-09-11 19:56:59 -0700

    logic for form post added

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
-// create contants for express and port
+// initialize constants
 const app = express();
 const port = 3000;
+const posts = [];
 
 // parse incoming requests and serve static files
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-// routing for homepage
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// routing for homepage (get)
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
+    // render index.ejs with posts array
+    res.render('index', {posts: posts});
+
 });
 
-// start the server
+// form submission (post)
+app.post("/", (req, res) => {
+    // create a new post object
+    const post = {
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content,
+        timestamp: new Date().toTimeString()
+    };
+
+    // add the post to the posts array
+    posts.push(post);
+
+    // redirect to homepage
+    res.redirect('/');
+
+});
+
+// start the server (listen)
 app.listen(port, () => {
+    // log message to the console to confirm server start
     console.log('Server running on port ${port}.');
+
 });
