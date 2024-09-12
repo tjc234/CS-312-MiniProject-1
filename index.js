4878000a5e374a31e4e4493a1f4feb921c28191d
tjc234 <tjc234@nau.edu>
2024-09-11 19:23:44 -0700

    comment index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+// create contants for express and port
 const app = express();
 const port = 3000;
 
+// parse incoming requests and serve static files
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+// routing for homepage
 app.get("/", (req, res) => {
     res.send("Hello, world!");
 });
 
+// start the server
 app.listen(port, () => {
     console.log('Server running on port ${port}.');
 });
