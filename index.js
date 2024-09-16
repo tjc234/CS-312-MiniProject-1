bac795a6ad27829f9218c1fcb9e9fa36729d1da6
tjc234 <tjc234@nau.edu>
2024-09-16 12:15:25 -0700

    final changes

--- index.js
+++ index.js
@@ -4,49 +4,49 @@ import bodyParser from 'body-parser';
 // initialize global variables
 const app = express();
 const port = 3000;
-let posts = [];
-let postToEdit = null;
+let posts = []; // array to hold posts
+let postToEdit = null; // variable to hold current post being editted
 
 // parse incoming requests and serve static files
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); 
 
 // routing for homepage (get)
 app.get("/", (req, res) => {
-    // render index.ejs with posts array and postToEdit
+    // render index.ejs with posts array and post to edit
     res.render('index', {posts: posts, postToEdit: postToEdit});
 
-    // reset to null after rendering
+    // reset post to edit to null after rendering
     postToEdit = null;
 });
 
 // form submission (post) 
-app.post("/", (req, res) => {
+app.post("/", (req, res) => { 
     // get the form data
     const { author, title, content, originalTitle } = req.body;
 
     // if the original title is not null post is being edited
     if ( originalTitle ) {
         // find the index of the post in the array
-        const index = posts.findIndex(post => post.title === originalTitle);
+        const index = posts.findIndex(post => post.title === originalTitle); 
 
         // if the post is found negative one is returned
         if (index !== -1) {
             // update the post in the array with the form data
             posts[index] = {
                 author: author,
-                title: title, 
-                content: content,
+                title: title,  
+                content: content, 
                 timestamp: new Date().toTimeString()
             };
         }
     // if original title valuse is null post is new
     } else {
-        // create a new post object with the form data
-        const post = {
+        // create a new post object with the form data 
+        const post = { 
             author: req.body.author,
             title: req.body.title,
             content: req.body.content,
@@ -54,7 +54,7 @@ app.post("/", (req, res) => {
         };
 
         // add the post to the array
-        posts.push(post);
+        posts.push(post); 
     }
 
     // redirect to homepage
@@ -93,4 +93,4 @@ app.listen(port, () => {
     // log message to the console to confirm server start
     console.log(`Server running on port ${port}.`);
 
-});
+}); 
